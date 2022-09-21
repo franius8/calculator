fae651cad7840c6a2772368f68441ff8ba63746c
franius8 <skawinskifranciszek@gmail.com>
2022-09-21 23:58:56 +0200

    Add functions performing operations

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
 const numberButtons = document.querySelectorAll('.numberbutton');
 const display = document.querySelector('#screendiv');
+const operandButtons = document.querySelectorAll('.operandbutton');
+const equalButton = document.querySelector('#equalbutton');
+let operationType = null;
+let firstNumber;
 
 numberButtons.forEach(button => {
     button.addEventListener('click', addToDisplay)
 });
 
+operandButtons.forEach(button => {
+    button.addEventListener('click', changeOperationType)
+});
+
+equalButton.addEventListener('click', performOperation)
+
+
 function addToDisplay() {
     const number = (this.id).substr(-1, 1);
-    console.log(number);
     const content = document.querySelector("#screendiv").innerText;
     const length = document.querySelector("#screendiv").innerText.length;
-    console.log(typeof content);
-    if (content === '0') {
+    if (content === '0' || operationType !== null) {
         display.textContent = number;
     } else if (length < 10) {
         display.textContent += number;
     } else {
         alert('Max length reached!')
+    }
+    operandButtons.forEach(button => button.classList.remove('active'));
+}
+
+function changeOperationType() {
+    operationType = this.id;
+    this.classList.add('active');
+    firstNumber = document.querySelector("#screendiv").innerText;
+    console.log(operationType);
+}
+
+function performOperation() {
+    const secondNumber = document.querySelector("#screendiv").innerText;
+    switch (operationType) {
+        case 'add':
+            display.textContent = +firstNumber + +secondNumber;
+            break;
+        case 'subtract':
+            display.textContent = +firstNumber - +secondNumber;
+            break;
+        case 'multiply':
+            display.textContent = +firstNumber * +secondNumber;
+            break;
+        case 'divide':
+            display.textContent = +firstNumber / +secondNumber;
+            break;
+        default:
+            break;
     }
 }
